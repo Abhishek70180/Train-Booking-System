a295aa7871d46139babd11c04aec31fe731fc6f5
qdevelopergithub <43872892+qdevelopergithub@users.noreply.github.com>
2024-04-26 22:17:05 +0530

    ye

--- Trainbookingsystem/Controllers/BookingDetailController.cs
+++ Trainbookingsystem/Controllers/BookingDetailController.cs
@@ -22,14 +22,21 @@ namespace Trainbookingsystem.Controllers
         {
             _context = context;            
         }
-
         public async Task<IActionResult> Index()
         {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var bookings = await _context.BookingTickets
-                .Where(b => b.UserId == userId)
-                .ToListAsync();
-            return View(bookings);
+            if (User.IsInRole("Admin"))
+            {
+                var bookings = await _context.BookingTickets.ToListAsync();
+                return View(bookings);
+            }
+            else
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var bookings = await _context.BookingTickets
+                    .Where(b => b.UserId == userId)
+                    .ToListAsync();
+                return View(bookings);
+            }
         }
 
         [HttpPost]
@@ -46,84 +53,37 @@ namespace Trainbookingsystem.Controllers
             _context.BookingTickets.Remove(booking);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        }     
 
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var booking = await _context.BookingTickets
-                .FirstOrDefaultAsync(b => b.BookingId == id && b.UserId == userId);
-
-            if (booking == null)
-            {
-                return NotFound();
-
-            }
-
-            return View(booking);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, BookingTicket booking)
-        {
-            if (id != booking.BookingId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(booking);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BookingExists(booking.BookingId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(booking);
-        }
 
         private bool BookingExists(int id)
         {
             return _context.BookingTickets.Any(b => b.BookingId == id);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Cancel(int id)
         {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var booking = await _context.BookingTickets
-                .FirstOrDefaultAsync(b => b.BookingId == id && b.UserId == userId);
+            var booking = await _context.BookingTickets.FindAsync(id);
 
             if (booking == null)
             {
                 return NotFound();
             }
+
             var train = await _context.Trains.FindAsync(booking.TrainId);
             if (train != null)
             {
                 train.AvailableSeats += booking.NumberOfTickets;
                 _context.Update(train);
             }
+
             _context.BookingTickets.Remove(booking);
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
         }
+
     }
 }
     
